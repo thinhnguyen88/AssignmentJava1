ba2cd0d382367fd83823b478c735d0d59beca022
Thinh Nguyen <nguyenducthinh210@gmail.com>
2017-02-13 19:11:43 +0700

    tao class Model

---
+++ src/fpt/java1/assignment/model/User.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fpt.java1.assignment.model;
+
+/**
+ *
+ * @author Thinh Nguyen
+ */
+public class User {
+    
+}
